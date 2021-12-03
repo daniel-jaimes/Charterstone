687c9be9b064d6a84341d1a5c01bb4e2bbf2c265
daniel--jaimes <48568101+daniel-jaimes@users.noreply.github.com>
2021-12-03 20:18:49 +0100

    creade methods
    readPlayers
    readLocations

--- src/Manager/Controller.java
+++ src/Manager/Controller.java
@@ -1,10 +1,10 @@
 package Manager;
 
 import Dao.InputReader;
+import Model.Location;
+import Model.Player;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.util.ArrayList;
 
 public class Controller {
     private static Controller controller;
@@ -16,18 +16,54 @@ public class Controller {
         return controller;
     }
     public void init(){
-        try(BufferedReader BR = new BufferedReader(InputReader.getData())){
-            String str;
-            boolean exit = false;
-            while ((str = BR.readLine()) != null && !exit){
-                System.out.println(str);
-                exit = commands(str);
+        InputReader inputReader = new InputReader();
+        ArrayList<Player> players;
+        ArrayList<Location> locals;
+        players = readPlayers(inputReader);
+        locals = readLocations(inputReader);
+    }
+
+    private ArrayList<Location> readLocations(InputReader inputReader) {
+        boolean exit = false;
+        ArrayList<Location> locals = new ArrayList<>();
+        do {
+            String str = inputReader.readLine();
+            String[] localization;
+            //SE ACABA
+            if(str.equals("##")) exit = true;
+            // ES UNA LOCALIZACION
+            if (Character.toLowerCase(str.charAt(0)) == Character.toLowerCase('L')
+                    && (localization = str.substring(1).split(" ")).length == 6) {
+                int num = Integer.parseInt(localization[0]);
+                char resourceRequired = localization[1].charAt(0);
+                int qRequired = Integer.parseInt(localization[2]);
+                char resourceObtained = localization[4].charAt(0);
+                int qObtained = Integer.parseInt(localization[5]);
+                locals.add(new Location(num, resourceRequired, qRequired, resourceObtained, qObtained));
             }
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
+        }while(!exit);
+        return locals;
     }
-    private boolean commands(String line){
-        return true;
+
+    private ArrayList<Player> readPlayers(InputReader inputReader) {
+        boolean exit = false;
+        ArrayList<Player> players = new ArrayList<>();
+        do {
+            String str = inputReader.readLine();
+            String[] player;
+            //SE ACABA
+            if(str.equals("##")) exit = true;
+            // ES UN JUGADOR
+            if (str.charAt(0) == '#' && (player = str.substring(1).split(" ")).length == 5){
+                int num = Integer.parseInt(player[0]);
+                String color = player[1];
+                int qCarbon = Integer.parseInt(player[2]);
+                int qTrigo = Integer.parseInt(player[3]);
+                int qWood = Integer.parseInt(player[4]);
+                players.add(new Player(num, color, qCarbon, qTrigo, qWood));
+            }
+        }while(!exit);
+        return players;
     }
+
 }
