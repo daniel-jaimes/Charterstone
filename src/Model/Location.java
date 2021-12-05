10af4752dab2c738f7523155da65f8745363ab5e
Daniel J <48568101+daniel-jaimes@users.noreply.github.com>
2021-12-05 19:49:36 +0100

    create methods: - toString - getNum - getNumPlayer
    And change types of atributes

--- src/Model/Location.java
+++ src/Model/Location.java
@@ -2,16 +2,41 @@ package Model;
 
 public class Location {
     private int num;
-    private char resourceRequired;
+    private Character resourceRequired;
     private int qRequired;
-    private char resourceObtained;
+    private Character resourceObtained;
     private int qObtained;
-
+    private Player player;
     public Location(int num, char resourceRequired, int qRequired, char resourceObtained, int qObtained) {
         this.num = num;
         this.resourceRequired = resourceRequired;
         this.qRequired = qRequired;
         this.resourceObtained = resourceObtained;
         this.qObtained = qObtained;
+    }
+
+    @Override
+    public String toString() {
+        String namePlayer;
+        if(player == null) namePlayer = "ninguno";
+        else namePlayer = player.getColor();
+        return "Localizacion [" +
+                "necesario=" + resourceRequired +
+                ", numNecesario=" + qRequired +
+                ", Obtenido=" + resourceObtained +
+                ", numObtenido=" + qObtained +
+                ", jugador=" + namePlayer +
+                "]";
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+    public int getNumPlayer(){
+        return player.getNum();
     }
 }
