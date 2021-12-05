426f2f4ce71a26c144d1212029761b64bf61b129
Daniel J <48568101+daniel-jaimes@users.noreply.github.com>
2021-12-05 19:51:29 +0100

    create methods: - toString - getNum - getColor
    add atributes

--- src/Model/Player.java
+++ src/Model/Player.java
@@ -6,11 +6,33 @@ public class Player {
     private int qCarbon;
     private int qTrigo;
     private int qWood;
+    private int coins;
+    private int score;
     public Player(int num, String color, int qCarbon, int qTrigo, int qWood){
         this.num = num;
         this.color = color;
         this.qCarbon = qCarbon;
         this.qTrigo = qTrigo;
         this.qWood = qWood;
+    }
+
+    @Override
+    public String toString() {
+        return "Jugador [" +
+                "color=" + color +
+                ", puntuacion=" + score +
+                ", madera=" + qWood +
+                ", carbon=" + qCarbon +
+                ", trigo=" + qTrigo +
+                ", monedas=" + coins +
+                "]";
+    }
+
+    protected String getColor() {
+        return color;
+    }
+
+    public int getNum() {
+        return num;
     }
 }
