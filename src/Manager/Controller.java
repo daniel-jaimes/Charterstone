19de570816c2aea26bcb05661fb04cdd2f670246
Daniel J <48568101+daniel-jaimes@users.noreply.github.com>
2021-12-05 19:53:57 +0100

    create methods: - readAction - action(I, R, M, S, L) adn there prints

--- src/Manager/Controller.java
+++ src/Manager/Controller.java
@@ -1,6 +1,7 @@
 package Manager;
 
 import Dao.InputReader;
+import Exceptions.LogicException;
 import Model.Location;
 import Model.Player;
 
@@ -8,6 +9,8 @@ import java.util.ArrayList;
 
 public class Controller {
     private static Controller controller;
+    private Game game;
+    private int numPrevPlayer;
     private Controller(){}
     public static Controller getInstance(){
         if(controller == null){
@@ -21,7 +24,115 @@ public class Controller {
         ArrayList<Location> locals;
         players = readPlayers(inputReader);
         locals = readLocations(inputReader);
+        readActions(inputReader, players, locals);
     }
+
+    private void readActions(InputReader inputReader, ArrayList<Player> players,
+                             ArrayList<Location> locals) {
+        boolean exit = false;
+        do{
+            String str = inputReader.readLine();
+            String[] actions;
+            if(str.equals("")) exit = true;
+            try {
+                checkAction(str, players, locals);
+            } catch (LogicException e) {
+                System.out.println(e.getMessage());
+
+            }
+        }while(!exit);
+    }
+
+    private void checkAction(String line, ArrayList<Player> players,
+                             ArrayList<Location> locals) throws LogicException {
+        String[] action = line.split(" ");
+        if((this.game == null) && !action[0].equalsIgnoreCase("I")) {
+            throw new LogicException(line, LogicException.INCORRECT_ACTION);
+        }
+        switch (action[0]) {
+            case "S":
+                actionS(line, action.length, players);
+                break;
+            case "I":
+                actionI(line, action.length, players, locals);
+                break;
+            case "M":
+                actionM(line, action, players, locals);
+                break;
+            case "R":
+                actionR(line, action, players, locals);
+                break;
+            case "L":
+                actionL(line, action.length, locals);
+                break;
+            default:
+                throw new LogicException(line, LogicException.INCORRECT_ACTION);
+        }
+    }
+    //ACTIONS
+    //ACTION L
+    private void actionL(String line, int actionLength, ArrayList<Location> locals) throws LogicException {
+        if(actionLength != 1) {
+            throw new LogicException(line, LogicException.INCORRECT_PAREAMETERS);
+        }
+        printLocals(locals);
+    }
+    private void printLocals(ArrayList<Location> locals) {
+        locals.forEach(loc -> System.out.println(loc.toString()));
+    }
+
+    //ACTION R
+    private void actionR(String line, String[] action, ArrayList<Player> players,
+                         ArrayList<Location> locals) throws LogicException {
+        if(action.length != 2) {
+            throw new LogicException(line, LogicException.INCORRECT_PAREAMETERS);
+        }
+        int numPlayer = Integer.parseInt(action[1]);
+        locals.forEach(loc -> {
+            if(loc.getNumPlayer() == numPlayer) loc.setPlayer(null);
+        });
+    }
+    //ACTION M
+    private void actionM(String line, String[] action, ArrayList<Player> players,
+                         ArrayList<Location> locals) throws LogicException {
+        if(action.length != 3) {
+            throw new LogicException(line, LogicException.INCORRECT_PAREAMETERS);
+        }
+        int numPlayer = Integer.parseInt(action[1]);
+        int posLocal = Integer.parseInt(action[2]);
+        //GET PLAYER
+        Player player = players.stream()
+                .filter(guy -> guy.getNum() == numPlayer)
+                .findFirst()
+                .get();
+        //GET LOCATION
+        Location local = locals.stream()
+                .filter(loc -> loc.getNum() == posLocal)
+                .findFirst()
+                .get();
+    }
+    //ACTION I
+    private void actionI(String line, int actionLength, ArrayList<Player> players,
+                         ArrayList<Location> locals) throws LogicException {
+        if(actionLength != 1) {
+            throw new LogicException(line, LogicException.INCORRECT_PAREAMETERS);
+        }
+        this.game = new Game(players, locals);
+        this.game.initGame();
+    }
+    //ACTION S
+    private void actionS(String line, int actionLength, ArrayList<Player> players)
+            throws LogicException {
+        if(actionLength != 1) {
+            throw new LogicException(line, LogicException.INCORRECT_PAREAMETERS);
+        }
+        printPlayers(players);
+    }
+    // S CODE - PRINT
+    private void printPlayers(ArrayList<Player> players) {
+        players.forEach(guy -> System.out.println(guy.toString()));
+    }
+
 
     private ArrayList<Location> readLocations(InputReader inputReader) {
         boolean exit = false;
